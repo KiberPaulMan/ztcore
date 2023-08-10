87a8ac05aff509b42713f4bd94a37d12d47476eb
Paul <twinencrypt@gmail.com>
2023-08-10 13:53:19 +0700

    add database settings

--- call_center/models.py
+++ call_center/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Comment(models.Model):
+    text = models.TextField(blank=True, null=True)
+
+
+class Status(models.Model):
+    COMMENT_CHOICES = (
+        ('status_1', 'Статус 1'),
+        ('status_2', 'Статус 2'),
+        ('status_3', 'Статус 3'),
+    )
+    title = models.CharField(choices=COMMENT_CHOICES, default='status_1', verbose_name='Статус')
